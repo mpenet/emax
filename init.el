a8d89db728818fea80de14c1e20c8aa19e3ad5bb
mpenet <max.penet@gmail.com>
2010-11-01 16:17:32 +0100

    font again + key navigation fn

--- init.el
+++ init.el
@@ -106,8 +106,8 @@
 
 ;; font
 ;(set-default-font "Envy Code R:pixelsize=15:foundry=unknown:weight=normal:slant=normal:width=normal:spacing=100:scalable=true")
-;(set-default-font "-xos4-terminus-medium-r-normal-*-16-*-*-*-*-*-*-1")
-(set-default-font  "Inconsolata:pixelsize=16:foundry=unknown:weight=normal:slant=normal:width=normal:spacing=100:scalable=true")
+(set-default-font "-xos4-terminus-medium-r-normal-*-16-*-*-*-*-*-*-1")
+;(set-default-font  "Inconsolata:pixelsize=16:foundry=unknown:weight=normal:slant=normal:width=normal:spacing=100:scalable=true")
 (setq ns-use-system-highlight-color nil)
 (setq ns-pop-up-frames nil)
 (global-font-lock-mode 1)
@@ -206,8 +206,34 @@ directory, select directory. Lastly the file is opened."
                                try-complete-file-name))
 
 ;; not really used anymore
-(global-set-key (kbd "M-<up>") 'backward-paragraph)
-(global-set-key (kbd "M-<down>") 'forward-paragraph)
+ (global-set-key (kbd "M-<up>") 'backward-paragraph)
+ (global-set-key (kbd "M-<down>") 'forward-paragraph)
+ (global-set-key (kbd "M-p") 'backward-paragraph)
+ (global-set-key (kbd "M-n") 'forward-paragraph)
+
+;; (defvar no-easy-keys-minor-mode-map (make-keymap)
+;;   "no-easy-keys-minor-mode keymap.")
+;; (let ((f (lambda (m)
+;;            `(lambda () (interactive)
+;;               (message (concat "No! use " ,m " instead."))))))
+;;   (dolist (l '(("<left>" . "C-b") ("<right>" . "C-f") ("<up>" . "C-p")
+;;                ("<down>" . "C-n")
+;;                ("<C-left>" . "M-f") ("<C-right>" . "M-b") ("<C-up>" . "M-{")
+;;                ("<C-down>" . "M-}")
+;;                ("<M-left>" . "M-f") ("<M-right>" . "M-b") ("<M-up>" . "M-{")
+;;                ("<M-down>" . "M-}")
+;;                ("<delete>" . "C-d") ("<C-delete>" . "M-d")
+;;                ("<M-delete>" . "M-d") ("<next>" . "C-v") ("<C-next>" . "M-x <")
+;;                ("<prior>" . "M-v") ("<C-prior>" . "M-x >")
+;;                ("<home>" . "C-a") ("<C-home>" . "M->")
+;;                ("<C-home>" . "M-<") ("<end>" . "C-e") ("<C-end>" . "M->")))
+;;     (define-key no-easy-keys-minor-mode-map
+;;       (read-kbd-macro (car l)) (funcall f (cdr l)))))
+;; (define-minor-mode no-easy-keys-minor-mode
+;;   "A minor mode that disables the arrow-keys, pg-up/down, delete
+;;   and backspace."  t " no-easy-keys"
+;;   'no-easy-keys-minor-mode-map :global t)
+;; (no-easy-keys-minor-mode 1)
 
 ;; make C-c C-c and C-c C-u work for comment/uncomment region in all modes
 (global-set-key (kbd "C-c C-c") 'comment-region)
