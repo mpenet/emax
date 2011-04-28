9f15a698e94b7c6fcd2ba2fe3984c261d2c00057
mpenet <max.penet@gmail.com>
2011-04-28 18:33:54 +0200

    swank-cdt setup

--- init.el
+++ init.el
@@ -45,38 +45,11 @@
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
+(eval-after-load "slime"
+  '(progn (slime-setup '(slime-repl))))
 (require 'slime)
-(slime-setup)
-
-(defun clojure-project (path)
-  "Setup classpaths for a clojure project and starts a new SLIME session.
-  Kills existing SLIME session, if any."
-  (interactive (list
-                (ido-read-directory-name
-                 "Project root: "
-                 (locate-dominating-file default-directory "src"))))
-  (require 'swank-clojure)
-  (when (get-buffer "*inferior-lisp*")
-    (kill-buffer "*inferior-lisp*"))
-  (add-to-list 'swank-clojure-extra-vm-args
-               (format "-Dclojure.compile.path=%s"
-                       (expand-file-name "target/classes/" path)))
-  (setq swank-clojure-binary nil
-        swank-clojure-jar-path (expand-file-name "target/dependency/" path)
-        swank-clojure-extra-classpaths
-        (append (mapcar (lambda (d) (expand-file-name d path))
-                        '("src/" "target/classes/" "test/" ))
-                (let ((lib (expand-file-name "lib" path)))
-                  (if (file-exists-p lib)
-                      (directory-files lib t ".jar$"))))
-        slime-lisp-implementations
-        (cons `(clojure ,(swank-clojure-cmd) :init swank-clojure-init)
-              (remove-if #'(lambda (x) (eq (car x) 'clojure))
-                         slime-lisp-implementations)))
-  (save-window-excursion
-    (slime)))
-
-(global-set-key (kbd "<f11>") 'clojure-project)
+(add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
+(global-set-key (kbd "<f11>") 'slime-connect)
 
 ;; tramp
 (setq tramp-default-method "ssh")
