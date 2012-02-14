09a2c812a711c9184c99b44c4ee1dd18ff23a85d
Max Penet <max.penet@gmail.com>
2012-02-14 14:05:19 +0100

    match \.ya?ml$

--- init.el
+++ init.el
@@ -168,7 +168,7 @@
 (setq auto-mode-alist (cons '("\\.less$" . css-mode) auto-mode-alist))
 
 (autoload 'yaml-mode "yaml-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
 
 ;; font
 (set-frame-font "-xos4-terminus-medium-r-normal-*-12-*-*-*-*-*-*-1")
