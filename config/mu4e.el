9331855b031fbf16ce4c86c0c86c7492910eb5ca
Max Penet <m@qbits.cc>
2013-08-19 22:40:36 +0200

    mu4e html2text setup + image preview

--- config/mu4e.el
+++ config/mu4e.el
@@ -1,11 +1,16 @@
 (autoload 'mu4e "mu4e" "" t)
 
+;; general
+(setq mu4e-view-show-images t
+      mu4e-view-image-max-width 800)
+
 ;; incoming
 (setq mu4e-drafts-folder "/[Gmail].Drafts"
       mu4e-sent-folder   "/[Gmail].Sent Mail"
       mu4e-trash-folder  "/[Gmail].Trash"
       ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
       mu4e-sent-messages-behavior 'delete
+
       ;; you can quickly switch to your Inbox -- press ``ji''
       ;; then, when you want archive some messages, move them to
       ;; the 'All Mail' folder by pressing ``ma''.
@@ -20,7 +25,9 @@
       mu4e-get-mail-command "offlineimap"
       mu4e-headers-leave-behavior 'apply
       mu4e-show-images t
-      mu4e-maildir (expand-file-name "~/mail"))
+      mu4e-maildir (expand-file-name "~/mail")
+      mu4e-update-interval 300
+      mu4e-html2text-command "html2text -utf8 -width 72")
 
 ;; outgoing
 (setq message-send-mail-function 'smtpmail-send-it
