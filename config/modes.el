3b816984ece48f0c10715edc477da69349dd2905
Max Penet <m@qbits.cc>
2014-08-11 15:08:53 +0200

    fix hask-interactive-mode

--- config/modes.el
+++ config/modes.el
@@ -156,8 +156,9 @@
   '(progn
           ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
           ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent))
-	  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-	  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)))
+     (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+     (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+     (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)))
 
 
 ;; buffers, project, files navigation (should refactor this into a mode)
