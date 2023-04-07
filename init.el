373fa7256dc0dd097c288faaa590cab36efaf2e5
Max Penet <mpenet@s-exp.com>
2023-04-07 08:53:27 +0200

    bump copyright year

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 
 ;;; License:
 
-;; Copyright (C) 2021 Max Penet
+;; Copyright (C) 2023 Max Penet
 
 ;; This program is free software: you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
